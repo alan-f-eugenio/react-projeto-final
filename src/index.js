017f92cd5fe924adda9244b0d0f5cab4c61c1665
unknown <alan.349@hotmail.com>
2021-09-02 22:17:48 -0300

    removed bootstrap.js to make everything with react

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
